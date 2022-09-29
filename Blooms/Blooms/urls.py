817c0626936e7bd4c3eb3a02bdce8fc15bc13b98
Haripriya K <haripriyakh2019@gmail.com>
2022-09-29 14:05:06 +0530

    commit before remote transfering

--- Blooms/Blooms/urls.py
+++ Blooms/Blooms/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+#try:
+urlpatterns = [ 
+    path('', include('user_sec.urls')),           
     path('admin/', admin.site.urls),
+    path('ad/', include('admin_p.urls')),
+    path('ct/', include('cart.urls')),
+    path('od/', include('order.urls')),
+    path('con/', include('content.urls')),
+    path('pf/', include('user_profile.urls')),
+    
 ]
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns +=path('__debug__/', include('debug_toolbar.urls')),
+# except Exception: 
+#     urlpatterns = [ ]
+           
